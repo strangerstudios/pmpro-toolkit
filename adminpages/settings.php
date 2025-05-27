ced4a1bc6142b4bdc8d75f68cc3dfe8554355c81
Dale Mugford <dmugford@paidmembershipspro.com>
2025-05-27 06:40:54 -0400

    Move performance testing to its own block
    Signed-off-by: Dale Mugford <dmugford@paidmembershipspro.com>

--- adminpages/settings.php
+++ adminpages/settings.php
@@ -158,91 +158,109 @@
 		</div>
 		<div class="pmpro_section_inside">
 			<table class="form-table">
-			<tbody>
-				<!--  Expire Memberships row  -->
-				<tr>
-					<th scope="row" valign="top">
-						<label for="ipn_debug"><?php esc_html_e( 'Gateway Callback Debug Email', 'pmpro-toolkit' ); ?></label>
-					</th>
-					<td>
-						<input type="email" id="ipn_debug" name="pmprodev_options[ipn_debug]" value="<?php echo esc_attr( $pmprodev_options['ipn_debug'] ); ?>" class="regular-text">
-						<p class="description">
-							<?php esc_html_e( 'Enter an email address to receive a debugging email every time the gateway processes data.', 'pmpro-toolkit' ); ?>
-						</p>
-					</td>
-				</tr>
-				<!-- another row but for Send Checkout Debug Email	 -->
-				<tr>
-					<th scope="row" valign="top">
-						<label for="checkout_debug_email"><?php esc_html_e( 'Send Checkout Debug Email', 'pmpro-toolkit' ); ?></label>
-					</th>
-					<td>
-						<select name="pmprodev_options[checkout_debug_when]">
-							<option value="" <?php selected( $pmprodev_options['checkout_debug_when'], '' ); ?>><?php esc_html_e( 'Never (Off)', 'pmpro-toolkit' ); ?></option>
-							<option value="on_checkout" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_checkout' ); ?>><?php esc_html_e( 'Yes, Every Page Load', 'pmpro-toolkit' ); ?></option>
-							<option value="on_submit" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_submit' ); ?>><?php esc_html_e( 'Yes, Submitted Forms Only', 'pmpro-toolkit' ); ?></option>
-							<option value="on_error" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_error' ); ?>><?php esc_html_e( 'Yes, Errors Only', 'pmpro-toolkit' ); ?></option>
-						</select>
-						<span><?php esc_html_e( 'to email:', 'pmpro-toolkit' ); ?></span>
-						<input type="email" id="checkout_debug_email" name="pmprodev_options[checkout_debug_email]" value="<?php echo esc_attr( $pmprodev_options['checkout_debug_email'] ); ?>">
-						<p class="description">
-							<?php esc_html_e( 'Send an email every time the Checkout page is hit.', 'pmpro-toolkit' ); ?>
-							<?php esc_html_e( 'This email will contain data about the request, user, membership level, order, and other information.', 'pmpro-toolkit' ); ?>
-						</p>
-					</td>
-				</tr>
-				<!-- Generate Checkout Info row -->
-				<tr>
-					<th scope="row" valign="top">
-						<label for="pmprodev_options[generate_info]"> <?php esc_html_e( 'Enable Generate Checkout Info Button', 'pmpro-toolkit' ); ?></label>
-					</th>
-					<td>
-						<input type="checkbox" id="pmprodev_options[generate_info]" name="pmprodev_options[generate_info]" value="1" <?php checked( $pmprodev_options['generate_info'], 1, true ); ?>>
-						<label for="pmprodev_options[generate_info]" class="description"><?php echo esc_html_e( 'Ability to generate checkout info when testing.', 'pmpro-toolkit' ); ?></label>
-					</td>
-				</tr>
-				<!-- Performance Testing Endpoint row -->
-				<tr>
-					<th scope="row" valign="top">
-						<label for="pmprodev_options[performance_endpoints]"><?php esc_html_e( 'Performance Testing Endpoints', 'pmpro-toolkit' ); ?></label>
-					</th>
-					<td>
-						<select name="pmprodev_options[performance_endpoints]" id="pmprodev_options[performance_endpoints]">
-							<option value="no" <?php selected( $pmprodev_options['performance_endpoints'], 'no' ); ?>><?php esc_html_e( 'No', 'pmpro-toolkit' ); ?></option>
-							<option value="read_only" <?php selected( $pmprodev_options['performance_endpoints'], 'read_only' ); ?>><?php esc_html_e( 'Read Only', 'pmpro-toolkit' ); ?></option>
-							<option value="read_write" <?php selected( $pmprodev_options['performance_endpoints'], 'read_write' ); ?>><?php esc_html_e( 'Read and Write', 'pmpro-toolkit' ); ?></option>
-						</select>
-						<p class="description">
-							<?php esc_html_e( 'Enable performance testing REST API endpoint for testing purposes.', 'pmpro-toolkit' ); ?>
-							<br>
-							<?php if ( ! empty( $pmprodev_options['performance_endpoints'] ) && $pmprodev_options['performance_endpoints'] !== 'no' ) : ?>
-								<br><br>
-								<strong><?php esc_html_e( 'Endpoint URLs:', 'pmpro-toolkit' ); ?></strong><br>
-								<code><?php echo esc_url( rest_url( 'toolkit/v1/(endpoint-name)' ) ); ?></code>
-								<br>
-								<em><?php esc_html_e( 'Use GET for read-only tests, POST for read-write tests (if enabled).', 'pmpro-toolkit' ); ?></em>
-								<br><br>
-								<strong><?php esc_html_e( 'NOTE:', 'pmpro-toolkit' ); ?></strong>
-								<?php
-								/* translators: 1: Example code define statement, 2: wp-config.php filename */
-								printf(
-									// translators: 1: define() code snippet, 2: wp-config.php filename
-									__( 'To enable full testing capability, make sure to add %1$s to your %2$s file.', 'pmpro-toolkit' ),
-									'<code>define( \'SAVEQUERIES\', true );</code>',
-									'<code>wp-config.php</code>'
-								);
-								?>						
-							<?php endif; ?>
-						</p>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+				<tbody>
+					<!--  Expire Memberships row  -->
+					<tr>
+						<th scope="row" valign="top">
+							<label for="ipn_debug"><?php esc_html_e( 'Gateway Callback Debug Email', 'pmpro-toolkit' ); ?></label>
+						</th>
+						<td>
+							<input type="email" id="ipn_debug" name="pmprodev_options[ipn_debug]" value="<?php echo esc_attr( $pmprodev_options['ipn_debug'] ); ?>" class="regular-text">
+							<p class="description">
+								<?php esc_html_e( 'Enter an email address to receive a debugging email every time the gateway processes data.', 'pmpro-toolkit' ); ?>
+							</p>
+						</td>
+					</tr>
+					<!-- another row but for Send Checkout Debug Email	 -->
+					<tr>
+						<th scope="row" valign="top">
+							<label for="checkout_debug_email"><?php esc_html_e( 'Send Checkout Debug Email', 'pmpro-toolkit' ); ?></label>
+						</th>
+						<td>
+							<select name="pmprodev_options[checkout_debug_when]">
+								<option value="" <?php selected( $pmprodev_options['checkout_debug_when'], '' ); ?>><?php esc_html_e( 'Never (Off)', 'pmpro-toolkit' ); ?></option>
+								<option value="on_checkout" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_checkout' ); ?>><?php esc_html_e( 'Yes, Every Page Load', 'pmpro-toolkit' ); ?></option>
+								<option value="on_submit" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_submit' ); ?>><?php esc_html_e( 'Yes, Submitted Forms Only', 'pmpro-toolkit' ); ?></option>
+								<option value="on_error" <?php selected( $pmprodev_options['checkout_debug_when'], 'on_error' ); ?>><?php esc_html_e( 'Yes, Errors Only', 'pmpro-toolkit' ); ?></option>
+							</select>
+							<span><?php esc_html_e( 'to email:', 'pmpro-toolkit' ); ?></span>
+							<input type="email" id="checkout_debug_email" name="pmprodev_options[checkout_debug_email]" value="<?php echo esc_attr( $pmprodev_options['checkout_debug_email'] ); ?>">
+							<p class="description">
+								<?php esc_html_e( 'Send an email every time the Checkout page is hit.', 'pmpro-toolkit' ); ?>
+								<?php esc_html_e( 'This email will contain data about the request, user, membership level, order, and other information.', 'pmpro-toolkit' ); ?>
+							</p>
+						</td>
+					</tr>
+					<!-- Generate Checkout Info row -->
+					<tr>
+						<th scope="row" valign="top">
+							<label for="pmprodev_options[generate_info]"> <?php esc_html_e( 'Enable Generate Checkout Info Button', 'pmpro-toolkit' ); ?></label>
+						</th>
+						<td>
+							<input type="checkbox" id="pmprodev_options[generate_info]" name="pmprodev_options[generate_info]" value="1" <?php checked( $pmprodev_options['generate_info'], 1, true ); ?>>
+							<label for="pmprodev_options[generate_info]" class="description"><?php echo esc_html_e( 'Ability to generate checkout info when testing.', 'pmpro-toolkit' ); ?></label>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
-<p class="submit">
-	<input name="savesettings" type="submit" class="button-primary" value="<?php esc_html_e( 'Save Settings', 'pmpro-toolkit' ); ?>">
-</p>
+	
+	<!-- Endpoints Performance Testing Section -->
+	<div class="pmpro_section" data-visibility="shown" data-activated="true">
+		<div class="pmpro_section_toggle">
+			<button class="pmpro_section-toggle-button" type="button" aria-expanded="true">
+				<span class="dashicons dashicons-arrow-up-alt2"></span>
+				<?php esc_html_e( 'Performance Testing Endpoints', 'pmpro-toolkit' ); ?>
+			</button>
+		</div>
+		<div class="pmpro_section_inside">
+			<table class="form-table">
+				<tbody>
+					<!--  Endpoints Performance Testing  -->
+					<tr>
+						<th scope="row" valign="top">
+							<label for="pmprodev_options[performance_endpoints]"><?php esc_html_e( 'Performance Testing Endpoints', 'pmpro-toolkit' ); ?></label>
+						</th>
+						<td>
+							<select name="pmprodev_options[performance_endpoints]" id="pmprodev_options[performance_endpoints]">
+								<option value="no" <?php selected( $pmprodev_options['performance_endpoints'], 'no' ); ?>><?php esc_html_e( 'No', 'pmpro-toolkit' ); ?></option>
+								<option value="read_only" <?php selected( $pmprodev_options['performance_endpoints'], 'read_only' ); ?>><?php esc_html_e( 'Read Only', 'pmpro-toolkit' ); ?></option>
+								<option value="read_write" <?php selected( $pmprodev_options['performance_endpoints'], 'read_write' ); ?>><?php esc_html_e( 'Read and Write', 'pmpro-toolkit' ); ?></option>
+							</select>
+							<p class="description">
+								<?php esc_html_e( 'Enable performance testing REST API endpoint for testing purposes.', 'pmpro-toolkit' ); ?>
+								<br>
+								<?php if ( ! empty( $pmprodev_options['performance_endpoints'] ) && $pmprodev_options['performance_endpoints'] !== 'no' ) : ?>
+									<br><br>
+									<strong><?php esc_html_e( 'Endpoint URLs:', 'pmpro-toolkit' ); ?></strong><br>
+									<code><?php echo esc_url( rest_url( 'toolkit/v1/(endpoint-name)' ) ); ?></code>
+									<br>
+									<em><?php esc_html_e( 'Use GET for read-only tests, POST for read-write tests (if enabled).', 'pmpro-toolkit' ); ?></em>
+									<br><br>
+									<strong><?php esc_html_e( 'NOTE:', 'pmpro-toolkit' ); ?></strong>
+									<?php
+									if ( !defined( 'SAVEQUERIES' ) && SAVEQUERIES ) { ?>
+										<?php esc_html_e( 'The SAVEQUERIES constant is already enabled.', 'pmpro-toolkit' ); ?>
+									<?php } else { ?>
+										<?php printf(
+										// translators: 1: define() code snippet, 2: wp-config.php filename
+										__( 'To enable full testing capability, make sure to add %1$s to your %2$s file.', 'pmpro-toolkit' ),
+										'<code>define( \'SAVEQUERIES\', true );</code>',
+										'<code>wp-config.php</code>'
+										); ?>
+									<?php } ?>					
+								<?php endif; ?>
+							</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<p class="submit">
+		<input name="savesettings" type="submit" class="button-primary" value="<?php esc_html_e( 'Save Settings', 'pmpro-toolkit' ); ?>">
+	</p>
 </form>
 
 <script type="text/javascript">
